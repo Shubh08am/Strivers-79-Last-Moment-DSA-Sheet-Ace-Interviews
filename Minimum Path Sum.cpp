dd23964217df631db55a60bd62e9c5727867b538
Shubham Yadav <shubh08am@gmail.com>
2023-09-02 12:16:38 +0530

    Update Minimum Path Sum.cpp

--- Minimum Path Sum.cpp
+++ Minimum Path Sum.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size() , n = grid[0].size() ; 
-        vector<vector<int>>dp(m,vector<int>(n,0)) ; 
+        vector<int>prev(n,0),curr(n,0) ; 
 
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
                 
-            if(i==m-1 && j==n-1) dp[m-1][n-1] = grid[m-1][n-1] ; 
+            if(i==m-1 && j==n-1) curr[n-1] = grid[m-1][n-1] ; 
             else{
                     int down = grid[i][j] , right = grid[i][j] ; 
                     if(i+1<m){
-                        down+= dp[i+1][j] ; 
+                        down+= prev[j] ; 
                     }
                     else{
-                        down+=1e9;
+                        down+=1e6;
                     }
                     if(j+1<n){
-                        right+=dp[i][j+1] ; 
+                        right+=curr[j+1] ; 
                     }  
                     else{
-                        right+=1e9;
+                        right+=1e6;
                     } 
-                dp[i][j] = min(down,right);
+                curr[j] = min(down,right);
                 }
             }
+        prev=curr;
         }
-    return dp[0][0];
+    return prev[0];
     }
 };
  
